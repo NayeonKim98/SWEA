8128c8df715d5886c7b49002d13f02adb56364bd
NaYeon Kim <0o01kny@gmail.com>
2025-02-23 19:33:17 +0900

    1860_bread_need_retry

--- (02.19) Queue-1/1860.py
+++ (02.19) Queue-1/1860.py
@@ -3,34 +3,20 @@
 from collections import deque
 
 import sys
-sys.stdin = open("1860.input.txt", "r")
+sys.stdin = open(r"C:\Users\twony\Desktop\SWEA\(02.19) Queue-1\1860.input.txt", "r", encoding="utf-8")
 
 T = int(input())
-for tc in range(1, 30+1):
-    N, M, K = map(int, input().split())
-    arrived_time = list(map(int, input().split()))
-    arrived_time_sorted = deque(sorted(arrived_time))
+for tc in range(1, T + 1):
+    N, M, K = map(int, input().split())  # N: 손님 수, M: 붕어빵 만드는 시간, K: M초마다 만드는 붕어빵 개수
+    arrived_time = sorted(map(int, input().split()))  # 손님 도착 시간을 정렬
 
-    result = 'Possible'
-    original_K = K
-
-    print(f'{N} {M} {K}')
-    print(arrived_time_sorted)
-
-    # K 개 만큼 만들었으면 0~K-1번 인덱스까지의 사람들 남은 시간 - M (que의 숫자들은 도착한 시간임!!!!!!!!!!)
-
-    while N > 0:                        # 먹을 수 있는 사람이 남아있을 때까지
-        if arrived_time_sorted[0] - M >= 0:    # 도착을 굽는 시간보다 더 늦게 하면
-            N -= 1                      # 먹을 수 있는 사람 감소
-            K -= 1                      # 만든 붕어빵 개수 감소
-            arrived_time_sorted.popleft()   # 먹은사람 퇴장
-            if K == 0:                  # 만약
-                K = original_K
-        else:                           # 도착 시간이 붕어빵 만드는 시간을 넘어서면
-            result = 'Impossible'
+    result = "Possible"
+    for i in range(N):
+        time = arrived_time[i]  # 현재 손님이 도착하는 시간
+        available_bread = (time // M) * K - i  # time까지 만들어진 붕어빵 개수 - 이미 제공한 손님 수
+        
+        if available_bread <= 0:  # 손님에게 줄 붕어빵이 부족하면 Impossible
+            result = "Impossible"
             break
 
-    if len(arrived_time_sorted) != 0:
-        result = 'Impossible'
-
-    print(f'#{tc} {result}')
+    print(f"#{tc} {result}")
