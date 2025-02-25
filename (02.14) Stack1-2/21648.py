6c4b5be972e09a5ec381befc71958236a10413cf
Nayeon98 <0o01kny@gmail.com>
2025-02-25 17:53:59 +0900

    4871_continue

--- (02.14) Stack1-2/21648.py
+++ (02.14) Stack1-2/21648.py
@@ -1,1 +1,15 @@
 # 4871. [파이썬 S/W 문제해결 기본] 4일차 - 그래프 경로
+
+import sys
+sys.stdin = open("21648.input.txt", "r")
+
+T = int(input())
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    adj_arr = [[] for _ in range(V + 1)]
+
+    for i in range(E):
+        v, w = map(int, input().split())
+        adj_arr[v].append(w)
+
+    print(f'#{tc} {adj_arr}')
--- (02.14) Stack1-2/5432.py
+++ (02.14) Stack1-2/5432.py
@@ -1,1 +1,23 @@
 # 쇠막대기 자르기
+
+import sys
+
+sys.stdin = open("5432.input.txt", "r")
+
+T = int(input())
+for tc in range(1, T + 1):
+    arr = input().strip()
+    stack = []
+    count = 0
+
+    for i in range(len(arr)):
+        if arr[i] == '(':
+            stack.append('(')  # 쇠막대기 시작
+        else:  # 닫는 괄호 `)`
+            stack.pop()  # 쇠막대기 끝 처리
+            if arr[i - 1] == '(':  # 레이저라면
+                count += len(stack)  # 현재 스택에 쌓인 쇠막대기 개수만큼 추가
+            else:  # 쇠막대기 끝이라면
+                count += 1  # 막대기가 끝났으므로 조각 1개 추가
+
+    print(f'#{tc} {count}')
