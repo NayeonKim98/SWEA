d75fe1028cddf8afc7e36fc6207cfb956e4f2297
Nayeon98 <0o01kny@gmail.com>
2025-03-07 08:39:13 +0900

    0306_Start1 전부 완료

--- 0306_Start1/1240.py
+++ 0306_Start1/1240.py
@@ -8,6 +8,7 @@ for tc in range(1, T + 1):
     N, M = map(int, input().split())
     code = [list(map(int, input())) for _ in range(N)]
 
+    # 1이 들어있는 행 찾기
     for i in range(N):
         for j in range(M):
             if code[i][j] == 1:
@@ -20,10 +21,11 @@ for tc in range(1, T + 1):
 
     decimal_dir = {
         '0001101': "0", '0011001': "1", '0010011': "2", '0111101': "3",
-        '1132': "4", '0110001': "5", '0101111': "6", '0111011': "7",
+        '0100011': "4", '0110001': "5", '0101111': "6", '0111011': "7",
         '0110111': "8", '0001011': "9"
     }
 
+    # 문자열을 7개씩 잘라서 10진수로 변환
     decimal_lst = []
     i = 0
     while i < len(binary_code):  # 문자열의 마지막에 다다를 때까지 반복
@@ -33,18 +35,20 @@ for tc in range(1, T + 1):
 
     decimal_code = list(map(int, decimal_lst))
 
+    # 홀수자리, 짝수자리 구분해서 합 구하기
     even_sum = 0
     odd_sum = 0
-    for num in decimal_code:
-        if num % 2 == 0:
-            even_sum += decimal_code[num]
+    for i, num in enumerate(decimal_code):
+        if (i+1) % 2 == 0:
+            even_sum += num
         else:
-            odd_sum += decimal_code[num]
+            odd_sum += num
 
+    # 결과 도출
     if (3 * odd_sum + even_sum) % 10 == 0:
         result = sum(decimal_code)
     else:
         result = 0
 
-    print((3 * odd_sum + even_sum))
-    print(result)
+    print(f'#{tc} {result}')
+
