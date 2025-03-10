e9b86b14dd4af2c71d17e4ace60b1d8a384018ca
Nayeon98 <0o01kny@gmail.com>
2025-03-11 08:35:11 +0900

    꼭 사칙연산(23884.py) 다시 풀어보기

--- 0305_Tree2/23884.py
+++ 0305_Tree2/23884.py
@@ -3,12 +3,23 @@
 import sys
 sys.stdin = open("23884.input.txt", "r")
 
-def inorder_traversal(tree, node):
-    if node in tree and len(tree[node]) == 3:  # 왼쪽 자식이 있으면 방문
-        inorder_traversal(tree, tree[node][1])
-    result.append(node)
-    if node in tree and len(tree[node]) == 3:  # 오른쪽 자식이 있으면 방문
-        inorder_traversal(tree, tree[node][2])
+def evaluate_expression(tree, node):  # 후위 순회 방식
+    if len(tree[node]) == 1:  # 숫자만 들어있으면, 그대로 숫자 반환
+        return tree[node][0]
+
+    operator, left, right = tree[node]  # 숫자, 연산자 같이 들어있는 경우
+    left_value = evaluate_expression(tree, left)
+    right_value = evaluate_expression(tree, right)
+
+    # 사칙연산
+    if operator == '+':
+        return left_value + right_value
+    elif operator == '-':
+        return left_value - right_value
+    elif operator == '*':
+        return left_value * right_value
+    elif operator == '/':
+        return left_value / right_value
 
 T = 10
 for tc in range(1, 11):
@@ -16,30 +27,18 @@ for tc in range(1, 11):
 
     tree = {}
 
-    for _ in range(N):  # 간선의 개수는 V - 1
-        edges = list(map(str, input().split()))
-        parent = int(edges[0])  # 첫번째 원소가 부모
-        if parent not in tree:
-            tree[parent] = []
-        if len(edges) == 4:
-            operator = edges[1]  # 사칙연산 따로 저장
-            tree[parent].append(operator)
-            # 연산 필요한 애들은 따로 저장
-            for j in range(2, len(edges)):
-                child = edges[j]
-                tree[parent].append(int(child))
-        else:  # 부모랑 숫자만 있으면 그냥 저장
-            child = edges[1]
-            tree[parent].append(int(child))
+    for _ in range(N):
+        edges = input().split()
+        parent = int(edges[0])  # 받은 edges의 첫 번째는 부모요소
 
-    result = []
-    stack = []
-    inorder_traversal(tree, 1)
-    for num in result:
-        for j in range(len(tree[num])):
-            if type(tree[num][j]) == int:
-                stack.append(tree[num][j])
-            elif type(tree[num][j]) == str:
-                pass
+        if len(edges) == 2:  # 연산자 없이 숫자만 받은 경우,
+            tree[parent] = [int(edges[1])]
+        else:  # 연산자도 같이 받은 경우라면,
+            operator = edges[1]
+            left_child = int(edges[2])
+            right_child = int(edges[3])
+            tree[parent] = [operator, left_child, right_child]
 
-    print(result)
+    result = evaluate_expression(tree, 1)
+
+    print(f'#{tc} {int(result)}')
--- 0310.ProblemSolving/10580.py
+++ 0310.ProblemSolving/10580.py
@@ -7,20 +7,13 @@ T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
     pole = [list(map(int, input().split())) for _ in range(N)]
+    cross_count = 0
 
-    a_list = []
-    b_list = []
-    cnt = 0
-    for ai, bi in pole:
-        a_list.append(ai)
-        b_list.append(bi)
+    pole.sort()
 
-    for i in range(N-1):
-        if a_list[i] > a_list[i + 1]:
-            cnt += 1
-            a_list[i], a_list[i + 1] = a_list[i + 1], a_list[i]
-        if b_list[i] > b_list[i + 1]:
-            cnt += 1
-            b_list[i], b_list[i + 1] = b_list[i + 1], b_list[i]
+    for i in range(N):
+        for j in range(i+1, N):
+            if pole[i][1] > pole[j][1]:  # A로 정렬된 상태에서(오름차순) 앞쪽의 B가 뒷쪽보다 크면 교차가 생김.
+                cross_count += 1
 
-    print(f'#{tc} {cnt}')
+    print(f'#{tc} {cross_count}')
